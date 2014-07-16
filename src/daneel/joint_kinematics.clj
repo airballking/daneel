5b67c73c287d271c3b5f4ade2515251b347b543e
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2014-07-16 12:41:35 +0200

    Split fk solvers into smaller functions. Altered test cases accordingly.

--- src/daneel/joint_kinematics.clj
+++ src/daneel/joint_kinematics.clj
@@ -4,17 +4,65 @@
   (:use clojure.core.matrix.operators)
   (:use daneel.transforms))
 
+;;;
+;;; PREDICATES TO CHECK FOR JOINT TYPES
+;;;
+
+(defn- joint-type-predicate
+  "Returns a predicate of arity 1 which checks whether a passed joint is
+  of type 'joint-type'."
+  [joint-type]
+  (fn [joint] (= (:type joint) joint-type)))
+
+(def fixed-joint? (joint-type-predicate :fixed))
+
+(def prismatic-joint? (joint-type-predicate :prismatic))
+
+(def revolute-joint? (joint-type-predicate :revolute))
+
+;;;
+;;; FORWARD KINEMATICS SOLVERS
+;;;
+
+(defn revolute-joint-fk-solver
+  "Returns a function of arity 1 which corresponds to the forward kinematics
+  of the input joint, assuming that it is a revolute joint."
+  [{:keys [axis origin]}]
+  (comp
+   (partial mmul origin)
+   (partial homogeneous-transform :rotation)
+   (partial axis-angle->rotation axis)))
+
+(defn prismatic-joint-fk-solver
+  "Returns a function of arity 1 which corresponds to the forward kinematics
+  of the input joint, assuming that it is a prismatic joint."
+  [{:keys [axis origin]}]
+  (comp
+   (partial mmul origin)
+   (partial homogeneous-transform :translation)
+   (partial * (normalise axis))))
+
+(defn fixed-joint-fk-solver
+  "Returns the transform corresponding to the input joint, assuming that
+  it is a fixed joint."
+  [{:keys [origin]}]
+  origin)
+
 (defn forward-kinematics-solver
-  "Returns a forward kinematics function for 'joint'. The returned function always
-  has arity 1."
-  [{:keys [type axis origin]}]
-  (case type
-    :revolute (comp
-                (partial mmul origin)
-                (partial homogeneous-transform :rotation)
-                (partial axis-angle->rotation axis))
-    :prismatic (comp
-                (partial mmul origin)
-                (partial homogeneous-transform :translation)
-                (partial * (normalise axis)))
-    :fixed (fn [_] origin)))
+  "Returns a forward kinematics function for 'joint'."
+  [joint]
+  (cond
+   (revolute-joint? joint) (revolute-joint-fk-solver joint)
+   (prismatic-joint? joint) (prismatic-joint-fk-solver joint)
+   (fixed-joint? joint) (fixed-joint-fk-solver joint))
+  ;; (case type
+  ;;   :revolute (comp
+  ;;               (partial mmul origin)
+  ;;               (partial homogeneous-transform :rotation)
+  ;;               (partial axis-angle->rotation axis))
+  ;;   :prismatic (comp
+  ;;               (partial mmul origin)
+  ;;               (partial homogeneous-transform :translation)
+  ;;               (partial * (normalise axis)))
+  ;;   :fixed (fn [_] origin))
+  )
--- test/daneel/joint_kinematics_test.clj
+++ test/daneel/joint_kinematics_test.clj
@@ -2,23 +2,55 @@
   (:require [clojure.test :refer :all]
             [daneel.joint-kinematics :refer :all]
             [daneel.transforms :refer :all]
-            [clojure.core.matrix :refer :all]))
+            [clojure.core.matrix :refer :all]
+            [clojure.core.matrix.operators :refer :all]))
 
-(deftest single-joint-fk-solver-test
-  (testing "FK solver for revolute joints"
+(deftest joint-predicates
+  (testing "Fixed joint predicates."
+    (are [joint result] (= (fixed-joint? joint) result)
+         {:type :fixed} true
+         {} false
+         {:type :prismatic} false
+         {:type :revolute} false))
+  (testing "Prismatic joint predicates."
+    (are [joint result] (= (prismatic-joint? joint) result)
+         {:type :prismatic} true
+         {} false
+         {:type :fixed} false
+         {:type :revolute} false))
+  (testing "Revolute joint predicates."
+    (are [joint result] (= (revolute-joint? joint) result)
+         {:type :revolute} true
+         {} false
+         {:type :prismatic} false
+         {:type :fixed} false)))
+
+(deftest revolute-joint-forward-kinematics-solvers
+  (testing "FK solver: revolute joint with identity origin."
     (are [joint joint-state transform]
-      (equals transform ((forward-kinematics-solver joint) joint-state))
-      ;; revolute joint without origin offset
+      (equals transform ((revolute-joint-fk-solver joint) joint-state))
+      ;; TEST 1
       ;; joint description..
-      {:type :revolute :axis [1 0 0] :origin (identity-matrix 4)}
+      {:axis [1 0 0] :origin (identity-matrix 4)}
       ;; joint state..
       Math/PI
       ;; joint transform..
       (homogeneous-transform :rotation (axis-angle->rotation [1 0 0] Math/PI))
       
-      ;; revolute joint with origin offset
+      ;; TEST 2
+      ;; joint description..
+      {:axis [0 0 1] :origin (identity-matrix 4)}
+      ;; joint state..
+      (- Math/PI)
+      ;; joint transform..
+      (homogeneous-transform :rotation (axis-angle->rotation [0 0 1] (- Math/PI)))))
+  
+  (testing "FK solver: revolute joint with non-identity origin."
+    (are [joint joint-state transform]
+      (equals transform ((revolute-joint-fk-solver joint) joint-state))
+      ;; TEST 1
       ;; joint description
-      {:type :revolute :axis [0 1 0]
+      {:axis [0 1 0]
        :origin (homogeneous-transform
                 :rotation (axis-angle->rotation [0 0 1] Math/PI)
                 :translation [0.1 0.2 0.3])}
@@ -28,22 +60,46 @@
       (mmul
        (homogeneous-transform :rotation (axis-angle->rotation [0 0 1] Math/PI)
                               :translation [0.1 0.2 0.3])
-       (homogeneous-transform :rotation (axis-angle->rotation [0 1 0] (/ Math/PI 4))))))
-  
-  (testing "FK solver for prismatic joints."
-    (are [joint joint-state transform]
-      (equals transform ((forward-kinematics-solver joint) joint-state))
-      ;; prismatic joint without offset
-      ;; joint description..
-      {:type :prismatic :axis [0 0 1] :origin (identity-matrix 4)}
-      ;; joint state..
-      0.05
-      ;; joint transform..
-      (homogeneous-transform :translation [0 0 0.05])
+       (homogeneous-transform :rotation (axis-angle->rotation [0 1 0] (/ Math/PI 4))))
 
-      ;; prismatic joint with offset
+      ;; TEST 2
+      ;; joint description
+      {:axis [1 1 1]
+       :origin (homogeneous-transform
+                :rotation (axis-angle->rotation [-1 0 1] (/ Math/PI 4))
+                :translation [0.1 0.2 0.3])}
+      ;; joint state
+      (/ Math/PI 16)
+      ;; joint transform
+      (mmul
+       (homogeneous-transform :rotation (axis-angle->rotation [-1 0 1] (/ Math/PI 4))
+                              :translation [0.1 0.2 0.3])
+       (homogeneous-transform :rotation (axis-angle->rotation [1 1 1] (/ Math/PI 16)))))))
+
+(deftest prismatic-joint-forward-kinematics-solver
+  (testing "FK solver: prismatic joint with identity origin."
+   (are [joint joint-state transform]
+     (equals transform ((prismatic-joint-fk-solver joint) joint-state))
+     ;; TEST CASE 1
+     ;; joint description..
+     {:axis [0 0 1] :origin (identity-matrix 4)}
+     ;; joint state..
+     0.05
+     ;; joint transform..
+     (homogeneous-transform :translation [0 0 0.05])
+     
+     ;; TEST CASE 2
+     ;; joint description..
+     {:axis [1 1 0] :origin (identity-matrix 4)}
+     ;; joint state..
+     (- 0.15)
+     ;; joint transform..
+     (homogeneous-transform :translation (* (- 0.15) (normalise [1 1 0])))))
+  (testing "FK solver: prismatic joint with non-identity origin."
+    (are [joint joint-state transform]
+      (equals transform ((prismatic-joint-fk-solver joint) joint-state))
       ;; joint description..
-      {:type :prismatic :axis [0 1 0]
+      {:axis [0 1 0]
        :origin (homogeneous-transform
                 :rotation (axis-angle->rotation [0 0 1] Math/PI)
                 :translation [0.1 0.2 0.3])}
@@ -54,27 +110,24 @@
        (homogeneous-transform
                 :rotation (axis-angle->rotation [0 0 1] Math/PI)
                 :translation [0.1 0.2 0.3])
-       (homogeneous-transform :translation [0 0.05 0]))))
-  
+       (homogeneous-transform :translation [0 0.05 0])))))
+
+(deftest fixed-joint-forward-kinematics-solver
   (testing "FK solver for fixed joints."
-    (are [joint joint-state transform]
-      (equals transform ((forward-kinematics-solver joint) joint-state))
-      ;; fixed joint without offset
+    (are [joint transform]
+      (equals transform (fixed-joint-fk-solver joint))
+      ;; TEST CASE 1
       ;; joint description..
       {:type :fixed :origin (identity-matrix 4)}
-      ;; joint state..
-      0.05 ; dont'care value
       ;; joint transform..
       (identity-matrix 4)
       
-      ;; fixed joint with offset
+      ;; TEST CASE 2
       ;; joint description..
       {:type :fixed
        :origin (homogeneous-transform
                 :rotation (axis-angle->rotation [0 0 1] Math/PI)
                 :translation [0.1 0.2 0.3])}
-      ;; joint state..
-      42 ; dont'care value
       ;; joint transform..
       (homogeneous-transform
                 :rotation (axis-angle->rotation [0 0 1] Math/PI)
